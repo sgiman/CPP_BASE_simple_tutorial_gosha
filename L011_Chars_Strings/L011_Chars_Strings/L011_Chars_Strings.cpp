d3b42e1a2c2cf00417fe33f139bef45d8b0bb884
Sergey Gasanov <sgimancs@gmail.com>
2022-08-24 18:04:15 +0300

    Update L011_Chars_Strings.cpp

--- L011_Chars_Strings/L011_Chars_Strings/L011_Chars_Strings.cpp
+++ L011_Chars_Strings/L011_Chars_Strings/L011_Chars_Strings.cpp
@@ -22,17 +22,17 @@ int main()
 	//setlocale(LC_ALL, "ru");				// <clocale> или
 	// setlocale(LC_CTYPE, "Russian");		     	// <clocale>
 	setlocale(LC_CTYPE, "rus");				// <clocale>
-	SetConsoleCP(1251);						// кодировка win-cp 1251 <Windows.h> - только для Windows
+	SetConsoleCP(1251);					// кодировка win-cp 1251 <Windows.h> - только для Windows
 	SetConsoleOutputCP(1251);				// кодировка win-cp 1251 для вывода текста на консоль
 
-	// char ch = 'a';										// один симлол
-	// char string[6] = "Hello";							// массив символов (строка) - (5 + 1) = 6 симвлов с нулём-терминатором '/0'. ИЛИ
+	// char ch = 'a';					// один симлол
+	// char string[6] = "Hello";				// массив символов (строка) - (5 + 1) = 6 симвлов с нулём-терминатором '/0'. ИЛИ
 	// char string[6] = { 'H', 'e', 'l', 'l', 'o', '/0' };	// стандарная инициализация массива 
-	// char string[] = "Hello World!";						// рекомендуется!
+	// char string[] = "Hello World!";			// рекомендуется!
 
 	char string[500];
 	cout << "Введите строку: ";
-	gets_s(string);				// gets - удалена и С++ в 2017
+	gets_s(string);			// gets - удалена из С++ в 2017
 	cout << string << endl;
 
 
